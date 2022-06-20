65b8e5d7080ab6e9c7b311c1299e793bda726dc1
unknown <jecanqui@bg.com.bo>
2022-06-20 19:58:53 -0400

    commit  inicial

---
+++ src/main/java/bg/com/bo/demoBG/DemoBgApplication.java
@@ -0,0 +1,13 @@
+package bg.com.bo.demoBG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBgApplication.class, args);
+	}
+
+}
