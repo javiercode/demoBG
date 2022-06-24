93c3053a8d64a58018fc30793ed0aaced93723b0
unknown <jecanqui@bg.com.bo>
2022-06-24 18:49:05 -0400

    comit images

---
+++ src/main/java/bg/com/bo/demoBG/service/dto/CamaraResponse.java
@@ -0,0 +1,13 @@
+package bg.com.bo.demoBG.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Response {
+    private Boolean estado;
+    private String mensaje;
+}
